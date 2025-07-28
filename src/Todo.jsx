381f161ea63ba94d30d3300cf57f6ba98493c3ec
Mohomed Afran <frmafran2004@gmail.com>
2025-07-28 12:35:48 +0530

    initial commit

---
+++ src/Todo.jsx
@@ -0,0 +1,120 @@
+import React, { useEffect, useState } from "react";
+import {
+  HiArrowUpTray,
+  HiMiniPencil,
+  HiMiniTrash,
+  HiPlus,
+} from "react-icons/hi2";
+import { uid } from "uid";
+import { onValue, ref, remove, set } from "firebase/database";
+import { db } from "./firebase";
+
+const Todo = () => {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [editMode, setEditMode] = useState(false);
+  const [todoId, setTodoId] = useState("");
+
+  useEffect(() => {
+    onValue(ref(db, "todos"), (snapshot) => {
+      setTodos([]);
+      const data = snapshot.val();
+      if (data) {
+        Object.values(data).forEach((todo) => {
+          setTodos((prev) => [...prev, todo]);
+        });
+      }
+    });
+  }, []);
+
+  const handleAddTodo = () => {
+    const todoId = uid();
+    set(ref(db, `todos/${todoId}`), {
+      todoId: todoId,
+      todo: todo,
+    });
+
+    setTodo("");
+  };
+
+  const deleteTodo = (todoId) => {
+    remove(ref(db, `todos/${todoId}`));
+  };
+
+  const editTodo = (todo) => {
+    setEditMode(true);
+    setTodo(todo.todo);
+    setTodoId(todo.todoId);
+  };
+
+  const updateTodo = () => {
+    set(ref(db, `todos/${todoId}`), {
+      todoId: todoId,
+      todo: todo,
+    });
+    setEditMode(false);
+    setTodo("");
+  };
+
+  return (
+    <div className="w-[100%] h-[100vh] bg-bg py-[5rem]">
+      <div className="w-[85%] lg:w-[70%] max-w-[600px] h-[100%] mx-auto py-12 px-8 rounded-xl bg-white shadow-lg">
+        <h1 className="text-2xl font-bold text-center mb-8 text-primary">
+          React Todo List
+        </h1>
+        <div className="flex flex-row items-center justify-between mb-12 gap-4 max-w-[100%]">
+          <input
+            type="text"
+            placeholder="Add a new task"
+            className="flex-grow border-2 border-secondary rounded-lg py-2 px-4 text-lg text-primary focus:shadow-md focus:outline-none focus:border-primary"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+          />
+          {!editMode ? (
+            <button
+              className="hover:bg-secondary/10 border-2 border-secondary text-secondary rounded-lg py-4 px-4 text-lg cursor-pointer"
+              onClick={handleAddTodo}
+            >
+              <HiPlus />
+            </button>
+          ) : (
+            <button
+              className="hover:bg-secondary/10 border-2 border-secondary text-secondary rounded-lg py-4 px-4 text-lg cursor-pointer"
+              onClick={updateTodo}
+            >
+              <HiArrowUpTray />
+            </button>
+          )}
+        </div>
+
+        {todos &&
+          todos.map((todo) => (
+            <div
+              className="flex flex-row justify-between items-center w-full py-2 px-4 bg-text-input/50 rounded-lg gap-4 mb-4"
+              key={todo.todoId}
+            >
+              <p className="text-lg text-primary font-medium flex-grow">
+                {todo.todo}
+              </p>
+              <div className="flex flex-row items-center text-lg gap-2 text-primary">
+                <button
+                  className="cursor-pointer"
+                  onClick={() => editTodo(todo)}
+                >
+                  <HiMiniPencil />
+                </button>
+                <button
+                  className="cursor-pointer"
+                  onClick={() => deleteTodo(todo.todoId)}
+                >
+                  <HiMiniTrash />
+                </button>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCUaf9LwOCmY85pop-U06EhHIZ_vp3IcpE",
+  authDomain: "react-todo-with-firebase-rtdb.firebaseapp.com",
+  projectId: "react-todo-with-firebase-rtdb",
+  storageBucket: "react-todo-with-firebase-rtdb.firebasestorage.app",
+  messagingSenderId: "1051721084126",
+  appId: "1:1051721084126:web:b27112f84c7804a326e64f",
+  measurementId: "G-Q39Q6050D4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
